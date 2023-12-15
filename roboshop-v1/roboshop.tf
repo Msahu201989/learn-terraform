35a58f2cc99b2898ebc10ad3f013085bce8b304f
Mukesh.Sahu <msahu201989@gmail.com>
2023-12-15 15:35:00 +0530

    Terraform learn

--- roboshop-v1/roboshop.tf
+++ roboshop-v1/roboshop.tf
@@ -25,6 +25,12 @@ resource "aws_instance" "mongodb" {
     Name = "mongodb"
   }
 }
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z03012723POIVATFERQS2"
+  name    = "frontend.msahu.online"
+  type    = "A"
+  records = [aws_instance.mongodb.private_ip]
+}
 
 resource "aws_instance" "catalogue" {
   ami           = "ami-03265a0778a880afb"
@@ -35,6 +41,13 @@ resource "aws_instance" "catalogue" {
     Name = "catalogue"
   }
 }
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z03012723POIVATFERQS2"
+  name    = "frontend.msahu.online"
+  type    = "A"
+  records = [aws_instance.catalogue.private_ip]
+}
+
 
 resource "aws_instance" "cart" {
   ami           = "ami-03265a0778a880afb"
@@ -45,6 +58,13 @@ resource "aws_instance" "cart" {
     Name = "cart"
   }
 }
+resource "aws_route53_record" "cart" {
+  zone_id = "Z03012723POIVATFERQS2"
+  name    = "frontend.msahu.online"
+  type    = "A"
+  records = [aws_instance.cart.private_ip]
+}
+
 
 resource "aws_instance" "redis" {
   ami           = "ami-03265a0778a880afb"
@@ -55,6 +75,13 @@ resource "aws_instance" "redis" {
     Name = "redis"
   }
 }
+resource "aws_route53_record" "redis" {
+  zone_id = "Z03012723POIVATFERQS2"
+  name    = "frontend.msahu.online"
+  type    = "A"
+  records = [aws_instance.redis.private_ip]
+}
+
 
 resource "aws_instance" "user" {
   ami           = "ami-03265a0778a880afb"
@@ -65,6 +92,13 @@ resource "aws_instance" "user" {
     Name = "user"
   }
 }
+resource "aws_route53_record" "user" {
+  zone_id = "Z03012723POIVATFERQS2"
+  name    = "frontend.msahu.online"
+  type    = "A"
+  records = [aws_instance.user.private_ip]
+}
+
 
 resource "aws_instance" "mysql" {
   ami           = "ami-03265a0778a880afb"
@@ -75,6 +109,13 @@ resource "aws_instance" "mysql" {
     Name = "mysql"
   }
 }
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z03012723POIVATFERQS2"
+  name    = "frontend.msahu.online"
+  type    = "A"
+  records = [aws_instance.mysql.private_ip]
+}
+
 
 resource "aws_instance" "shipping" {
   ami           = "ami-03265a0778a880afb"
@@ -85,6 +126,13 @@ resource "aws_instance" "shipping" {
     Name = "shipping"
   }
 }
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z03012723POIVATFERQS2"
+  name    = "frontend.msahu.online"
+  type    = "A"
+  records = [aws_instance.shipping.private_ip]
+}
+
 
 resource "aws_instance" "rabbitmq" {
   ami           = "ami-03265a0778a880afb"
@@ -96,6 +144,13 @@ resource "aws_instance" "rabbitmq" {
   }
 }
 
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z03012723POIVATFERQS2"
+  name    = "frontend.msahu.online"
+  type    = "A"
+  records = [aws_instance.rabbitmq.private_ip]
+}
+
 resource "aws_instance" "payment" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
@@ -104,4 +159,11 @@ resource "aws_instance" "payment" {
   tags   = {
     Name = "payment"
   }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = "Z03012723POIVATFERQS2"
+  name    = "frontend.msahu.online"
+  type    = "A"
+  records = [aws_instance.payment.private_ip]
 }
