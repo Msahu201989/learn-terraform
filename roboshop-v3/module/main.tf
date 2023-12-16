d32685985a4cded839402b720b8ffd99b56e02cc
Mukesh.Sahu <msahu201989@gmail.com>
2023-12-16 11:48:06 +0530

    Terraform learn

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -7,10 +7,6 @@ resource "aws_instance" "instance" {
     Name = var.name
   }
 
-  provisioner "local-exec" {
-    command = "sleep 60"
-  }
-
 }
 
 
@@ -22,4 +18,18 @@ resource "aws_route53_record" "record" {
   records = [aws_instance.instance.private_ip]
 }
 
+resource "null_resource" "ansible" {
 
+  depends_on = [
+    aws_route53_record.record
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/roboshop24-ansible
+git pull
+sleep 30
+ansible-playbook -i ${var.name}.msahu.online, main.yml -e ansible_user=centos -e anisble_password=DevOps321 -e component_name=${var.name}
+EOF
+  }
+}
