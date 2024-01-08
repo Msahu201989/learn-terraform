9c3c7ad3675e99a7133578aedc76e743cfe0dade
Mukesh.Sahu <msahu201989@gmail.com>
2024-01-08 12:12:41 +0530

    Terraform

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -2,7 +2,15 @@ resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
 
+ root_block_device {
+    volume_size = 15
+  }
+
   tags   = {
     Name = "Test"
   }
 }
+
+
+
+
